b509ac154be4a4fb09c3da31fde23b1d31dde023
Olle Kaiser <olle.kaiser@learnet.se>
2019-09-11 10:12:44 +0200

    Implemented update(), toString() and created the Update class

--- src/Message.java
+++ src/Message.java
@@ -1,15 +1,44 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Message {
-    private String message, authhor;
-    private Date createdAt, updatedAtDate;
+    private String message, author;
+    private Date createdAt, updatedAt;
+    ArrayList<Update> updates;
+
+    class Update{
+        private String author, previousMessage;
+        private Date updatedAt;
+        public Update(String author, String previousMessage){
+            this.author = author;
+            this.previousMessage = previousMessage;
+            this.updatedAt = new Date();
+        }
+
+        public String getPreviousMessage(){
+            return this.previousMessage;
+        }
+
+        public String getAuthor(){
+            return this.author;
+        }
+
+        public Date getDate(){
+            return this.updatedAt;
+        }
+    }
 
     /**
      * Creates a new message
      * @param message
      */
     public Message(String message){
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.message = message;
+        this.author = System.getProperty("user.name");
 
+        updates = new ArrayList<Update>();
     }
 
     /**
@@ -17,12 +46,20 @@ public class Message {
      * @param message New message
      */
     public void update(String message){
-
+        String newAuthor = System.getProperty("user.name");
+        this.updates.add(new Update(author, this.message));
+        this.message = message;
+        this.author = author;
     }
 
     @Override
     public String toString(){
-
-        return "";
+        String build = this.author + ": " + this.message + " CREATED: " + this.createdAt.toString();
+        if(updates.size() > 0) build += "\nORIGINAL BY  " + this.author + ": " + updates.get(0).getPreviousMessage() + " CREATED: " + this.createdAt.toString();
+        for(int i = 0; i < updates.size(); i++){
+            Update update = updates.get(i);
+            build += "\nREVISION BY  " + update.getAuthor() + ": " + (updates.size()-1 == i ? this.message : update.getPreviousMessage()) + " UPDATED: " + update.getDate();
+        }
+        return  build;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
+        Message testMessage = new Message("Hello world!");
+                testMessage.update("Oh no");
 
+        System.out.println(testMessage.toString());
     }
 }
