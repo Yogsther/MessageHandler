10023b0f88a1d986f0968f0e76efcf1260da9b26
Olle Kaiser <olle.kaiser@learnet.se>
2019-09-11 09:41:02 +0200

    Added message structure

---
+++ src/Message.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class Message {
+    private String message, authhor;
+    private Date createdAt, updatedAtDate;
+
+    /**
+     * Creates a new message
+     * @param message
+     */
+    public Message(String message){
+
+    }
+
+    /**
+     * Updates the message content
+     * @param message New message
+     */
+    public void update(String message){
+
+    }
+
+    @Override
+    public String toString(){
+
+    }
+}
